a3cc7bdbfecbad6299051ce346cad094d6346296
Kumuditha Perera <kumudithaperera@gmail.com>
2019-07-17 04:03:42 +0530

    added standered assests

---
+++ Spotlight Rush/Assets/Scripts/movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class movement : MonoBehaviour    
+{
+    private Rigidbody2D rd;
+    private float dirX;
+    private float movementSpeed = 400f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = CrossPlatformInputManager.GetAxis("Horizontal") * movementSpeed;
+        rd.velocity = new Vector2(dirX, 0f);
+    }
+}
